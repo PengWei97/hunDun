e7366a3cdf9ede35d6463789a714186dbc78025c
PengWei97 <1248110286@qq.com>
2024-10-17 16:18:00 +0800

    更新p22有无孪晶脚本

--- scripts/p22-Ti-withOrWithout-twins-2024/experiments/p22_exp01_draw_IPF_maps_from_crc.m
+++ scripts/p22-Ti-withOrWithout-twins-2024/experiments/p22_exp01_draw_IPF_maps_from_crc.m
@@ -19,7 +19,7 @@ outputDirCtf = '.\data\p22_Ti_withOrWithout_twins\exp_data\excerpt_ctf\';
 
 % which files to be imported
 % fname = [pname '\Ti_with_twins_v2.crc'];
-fname = [pname '\Ti_without_twins_v2.crc'];
+fname = [pname '\Ti_with_twins_v2_600du_0mins.crc'];
 
 %% Import the Data
 timePoints = [0, 10, 15, 30, 60];
@@ -28,15 +28,15 @@ for iTime = 1:1 %length(timePoints)
   ebsdData = EBSD.load(fname,CS,'interface','crc',...
     'convertSpatial2EulerReferenceFrame'); % convertSpatial2EulerReferenceFrame convertEuler2SpatialReferenceFrame
 
-  [grains, ebsdData] = identifyAndSmoothGrains(ebsdData, 2.0 * degree, 10, 3.0);
+  % % [grains, ebsdData] = identifyAndSmoothGrains(ebsdData, 2.0 * degree, 10, 3.0);
   % rot=rotation('Euler', 0*degree, 90*degree, 0*degree);
   % ebsdData=rotate(ebsdData,rot,'keepXY');
-  % plot(ebsdData, ebsdData.orientations)
-  idFigure = 1;
-  plotIPFMap(idFigure, ebsdData, grains);
+  plot(ebsdData, ebsdData.orientations, '')
+  % idFigure = 1;
+  % plotIPFMap(idFigure, ebsdData, grains);
 
-  % Output processed data in CTF format
-  outputFile = fullfile(outputDirCtf, sprintf('Ti_without_twins_%dmins_excerpt2.ctf', timePoints(iTime)));
-  export_ctf(ebsdData, outputFile);
+  % % Output processed data in CTF format
+  % outputFile = fullfile(outputDirCtf, sprintf('Ti_without_twins_%dmins_excerpt2.ctf', timePoints(iTime)));
+  % export_ctf(ebsdData, outputFile);
 end
-% E:\Github\MyRhinoLab\scripts\p22-Ti-withOrWithout-twins-2024\experiments\exp01_draw_IPF_maps_from_crc.m
+% E:\Github\MyRhinoLab\scripts\p22-Ti-withOrWithout-twins-2024\experiments\p22_exp01_draw_IPF_maps_from_crc.m
